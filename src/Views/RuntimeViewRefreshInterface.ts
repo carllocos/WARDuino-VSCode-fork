8292a6f5fce71c3d7b5e0a1ec74246cfa6c19374
carllocos <23368410+carllocos@users.noreply.github.com>
2024-03-03 22:02:19 +0100

    change view refresh interface

--- src/Views/RuntimeViewRefreshInterface.ts
+++ src/Views/RuntimeViewRefreshInterface.ts
@@ -1,8 +1,10 @@
+import { RemoteDebuggerBackend } from '../DebugSession/DebuggerBackend';
 import { OldRuntimeState } from '../State/RuntimeState';
 import { Context} from '../State/context';
 
 export interface RuntimeViewRefreshInterface {
 
+    setCurrentDBG(dbg: RemoteDebuggerBackend): void;
     oldRefreshView(runtimeState?: OldRuntimeState): void;
     refreshView(runtimeState?: Context): void;
 
