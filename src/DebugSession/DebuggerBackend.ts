ff16b8904aa1e0f739618071141c4f3ee31b9a5f
carllocos <23368410+carllocos@users.noreply.github.com>
2024-03-03 22:07:37 +0100

    refactor way of spawning devices

--- src/DebugSession/DebuggerBackend.ts
+++ src/DebugSession/DebuggerBackend.ts
@@ -1,9 +1,9 @@
-import { DeviceManager, DeploymentMode, SourceCodeLocation, SourceMap, StateRequest, WARDuinoVM, WasmState, PlatformBuilderConfig, Platform, BoardFQBN, DeviceConfigArgs, BoardBaudRate, VMConfigArgs, listAvailableBoards, WASM, OutOfPlaceMode } from 'wasmito';
+import { DeviceManager, DeploymentMode, SourceCodeLocation, SourceMap, StateRequest, WARDuinoVM, WasmState, PlatformBuilderConfig, Platform, BoardFQBN, DeviceConfigArgs, BoardBaudRate, VMConfigArgs, WASM, OutOfThingsMonitor, InputMode } from 'wasmito';
 import {EventEmitter} from 'events';
 import { Context } from '../State/context';
-import { DebuggingMode, UserConfig, createVMConfig as createVMConfigArgs} from '../DebuggerConfig';
-import { Source, Breakpoint as VSCodeBreakpoint } from 'vscode-debugadapter';
-import { Breakpoint, OutOfPlaceVM} from 'wasmito';
+import { DebuggingMode, UserDeviceConfig, UserEdwardDebuggingConfig, UserMCUConnectionConfig, UserOutOfThingsDebuggingConfig, UserRemoteDebuggingConfig } from '../DebuggerConfig';
+import {  Source } from 'vscode-debugadapter';
+import { Breakpoint as WasmBreakpoint, OutOfPlaceVM} from 'wasmito';
 
 export class BackendDebuggerEvent {
     public static readonly StateUpdated: string = 'state updated';
@@ -30,11 +30,12 @@ export class BackendDebuggerEvent {
     // public static readonly flashingFailure: string = 'Flashing failed';
 }
 
-export class BreakpointBackend extends VSCodeBreakpoint {
-    private readonly _bp: Breakpoint;
-    constructor(bp: Breakpoint, source: Source){
-        super(true, bp.sourceCodeLocation.linenr, bp.sourceCodeLocation.columnStart, source);
+export class BreakpointBackend  {
+    private readonly _bp: WasmBreakpoint;
+    private readonly _source: Source;
+    constructor(bp: WasmBreakpoint, source: Source){
         this._bp = bp;
+        this._source = source;
     }
 
     get linenr(): number{
@@ -42,11 +43,15 @@ export class BreakpointBackend extends VSCodeBreakpoint {
     }
 
     get sourceCodeLocation(): SourceCodeLocation {
-        return this.sourceCodeLocation;
+        return this.bp.sourceCodeLocation;
     }
 
-    get bp(): Breakpoint {
+    get bp(): WasmBreakpoint {
         return this._bp;
+    }
+
+    get source(): Source {
+        return this._source;
     }
 
 
@@ -55,18 +60,39 @@ export class BreakpointBackend extends VSCodeBreakpoint {
     }
 }
 
+
+export enum RunningState {
+    paused,
+    running
+}
+
+
+export interface DbgOptArgs {
+    initialContext?: Context
+    initialRunningState?: RunningState
+    isOutOfThingsDebugger?: boolean
+}
+
 export class RemoteDebuggerBackend extends EventEmitter {
 
-    private readonly vm: WARDuinoVM;
+    public readonly targetVM: WARDuinoVM;
+    public readonly debuggingMode: DebuggingMode;
     private context: Context;
 
     private _breakpoints: BreakpointBackend[];
 
-    constructor(vm: WARDuinoVM){
+    private _monitor?: OutOfThingsMonitor;
+    private _runningState: RunningState;
+    private _isOutOfThingsDBG: boolean;
+
+    constructor(vm: WARDuinoVM, debuggingMode: DebuggingMode, opts?: DbgOptArgs){
         super();
-        this.vm = vm;
-        this.context = new Context(new WasmState({}), this.vm.getSourceMap()!);
+        this.targetVM = vm;
+        this.debuggingMode = debuggingMode;
+        this.context = opts?.initialContext ?? new Context(new WasmState({}), this.targetVM.getSourceMap()!);
         this._breakpoints = [];
+        this._runningState = opts?.initialRunningState ?? RunningState.paused;
+        this._isOutOfThingsDBG = opts?.isOutOfThingsDebugger ?? false;
     }
 
 
@@ -74,43 +100,77 @@ export class RemoteDebuggerBackend extends EventEmitter {
         return this._breakpoints;
     }
 
+    set breakpoints(bps: BreakpointBackend[]) {
+        this._breakpoints = bps;
+    }
+
+    isOOTDBG(): boolean {
+        return this._isOutOfThingsDBG;
+    }
+
+    isPaused(): boolean{
+        return this._runningState === RunningState.paused;
+    }
+
+    hasOOTMonitor(): boolean{
+        return this._monitor !== undefined;
+    }
+
+    get monitor(): OutOfThingsMonitor {
+        if(this._monitor === undefined){
+            throw new Error('monitor was not set');
+        }
+        return this._monitor;
+    }
+
+
     async handleEvent(eventIndex: number): Promise<void>{
         // manually handling event is only for OutOfPlace debugging
-        if(!(this.vm instanceof OutOfPlaceVM) || this.vm.eventsToHandle.length === 0){
+        if(!(this.targetVM instanceof OutOfPlaceVM) || this.targetVM.eventsToHandle.length === 0){
             return;
         }
 
-        const ev = this.vm.eventsToHandle[0];
-        const handled = await this.vm.handleEvent(eventIndex);
+        const ev = this.targetVM.eventsToHandle[0];
+        const handled = await this.targetVM.handleEvent(eventIndex);
         if(!handled){
             throw Error('Event could not be handled');
         }
         
-        this.emit(BackendDebuggerEvent.EventHandled, ev, this.vm.eventsToHandle);
+        this.emit(BackendDebuggerEvent.EventHandled, ev, this.targetVM.eventsToHandle);
+    }
+
+    async setMonitor(monitor: OutOfThingsMonitor): Promise<void>{
+        this._monitor = monitor;
+        await monitor.setup();
     }
 
     close(): Promise<boolean> {
-        return this.vm.close();
+        return this.targetVM.close();
     }
 
     connect(timeout?: number): Promise<boolean> {
-        return this.vm.connect(timeout);
+        return this.targetVM.connect(timeout);
     }
 
     disconnect(): Promise<boolean> {
-        return this.vm.disconnect();
+        return this.targetVM.disconnect();
     }
 
-    run(timeout?: number): Promise<boolean> {
-        return this.vm.run(timeout);
+    async run(timeout?: number): Promise<boolean> {
+        const running =  await this.targetVM.run(timeout);
+        if(running){
+            this._runningState = RunningState.running;
+        }
+        return running;
     }
 
-    pause(timeout?: number): Promise<void> {
-        return this.vm.pause(timeout);
+    async pause(timeout?: number): Promise<void> {
+        await this.targetVM.pause(timeout);
+        this._runningState = RunningState.paused;
     }
 
     async step(timeout?: number): Promise<void> {
-        await this.vm.step(timeout);
+        await this.targetVM.step(timeout);
         await this.refreshState();
         return;
     }
@@ -119,20 +179,20 @@ export class RemoteDebuggerBackend extends EventEmitter {
         sourceCodePath: string,
         timeout?: number,
     ): Promise<boolean> {
-        return this.vm.uploadSourceCode(sourceCodePath, timeout);
+        return this.targetVM.uploadSourceCode(sourceCodePath, timeout);
     }
 
     proxify(timeout?: number): Promise<void> {
-        return this.vm.proxify(timeout);
+        return this.targetVM.proxify(timeout);
     }
 
     getSourceMap(): SourceMap {
-        return this.vm.sourceMap;
+        return this.targetVM.sourceMap;
     }
 
 
     private onBreakpointReached(state: WasmState): void {
-        const sourceMap = this.vm.sourceMap;
+        const sourceMap = this.targetVM.sourceMap;
         this.context =  new Context(state, sourceMap);
         this.emit(BackendDebuggerEvent.BreakpointReached, this.context, this.context.getCurrentSourceCodeLocation()!);
     }
@@ -140,8 +200,8 @@ export class RemoteDebuggerBackend extends EventEmitter {
 
     public onNewEvent(ev: WASM.Event): void {
         let allEvents: WASM.Event[] = this.context.events.values;
-        if( this.vm instanceof OutOfPlaceVM){
-            allEvents = this.vm.eventsToHandle;
+        if( this.targetVM instanceof OutOfPlaceVM){
+            allEvents = this.targetVM.eventsToHandle;
         }else{
             allEvents.push(ev);
         }
@@ -149,14 +209,13 @@ export class RemoteDebuggerBackend extends EventEmitter {
     }
 
     private async addBreakpoint(sourceCodeLocation: SourceCodeLocation): Promise<boolean>{
-        const stateOnBp = this.stateToRequest();
-        const bp = new Breakpoint(sourceCodeLocation, stateOnBp);
+        const bp = new WasmBreakpoint(sourceCodeLocation);
         bp.subscribe(this.onBreakpointReached.bind(this));
-        if(!await this.vm.addBreakpoint(bp)){
+        if(!await this.targetVM.addBreakpoint(bp)){
             return false;
         }
 
-        const sm = this.vm.sourceMap;
+        const sm = this.targetVM.sourceMap;
         const source = new Source(sm.sourceCodeFileName, sm.sourceCodeFilePath);
         this.breakpoints.push(new BreakpointBackend(bp, source));
         return true;
@@ -164,7 +223,7 @@ export class RemoteDebuggerBackend extends EventEmitter {
 
     private async removeBreakpoint(sourceCodeLocation: SourceCodeLocation, timeout?: number): Promise<boolean> {
         let bpPosition = -1;
-        const bpToRemove = new Breakpoint(sourceCodeLocation);
+        const bpToRemove = new WasmBreakpoint(sourceCodeLocation);
 
         const bp = this.breakpoints.find((bp, idx) =>{
             bpPosition = idx;
@@ -174,7 +233,7 @@ export class RemoteDebuggerBackend extends EventEmitter {
         if(bp === undefined){
             return true;
         }
-        const success =  await this.vm.removeBreakpoint(bp.bp, timeout);
+        const success =  await this.targetVM.removeBreakpoint(bp.bp, timeout);
         if(success){
             this.breakpoints.splice(bpPosition, 1);
         }
@@ -190,17 +249,17 @@ export class RemoteDebuggerBackend extends EventEmitter {
 
     async refreshState(): Promise<void> {
         const state = this.stateToRequest();
-        const response: WasmState = await this.vm.sendRequest(state);
+        const response: WasmState = await this.targetVM.sendRequest(state);
         const sourceMap = this.getSourceMap();
         this.context =  new Context(response, sourceMap);
-        if(this.vm instanceof OutOfPlaceVM){
-            this.context.events = this.vm.eventsToHandle;
+        if(this.targetVM instanceof OutOfPlaceVM){
+            this.context.events = this.targetVM.eventsToHandle;
         }
         this.emit(BackendDebuggerEvent.StateUpdated, this.context);
     }
 
 
-    getCurrentContext(): Context | undefined {
+    getCurrentContext(): Context {
         return this.context;
     }
 
@@ -234,12 +293,11 @@ export class RemoteDebuggerBackend extends EventEmitter {
     }
 }
 
-async function createPlatformConfig(config: UserConfig, vmConfigArgs: VMConfigArgs): Promise<PlatformBuilderConfig>{
+async function createPlatformConfig(config: UserMCUConnectionConfig, vmConfigArgs: VMConfigArgs): Promise<PlatformBuilderConfig>{
 
     const deviceConfigArgs: DeviceConfigArgs = {
-        // TODO handle name giving in toolkit
         name: config.boardName as string,
-        deploymentMode: config.target
+        deploymentMode: DeploymentMode.MCUVM
     };
     const boardFQN: BoardFQBN = {
         fqbn: config.fqbn as string,
@@ -248,48 +306,102 @@ async function createPlatformConfig(config: UserConfig, vmConfigArgs: VMConfigAr
     return new PlatformBuilderConfig(Platform.Arduino, config.baudrate as BoardBaudRate, boardFQN, deviceConfigArgs, vmConfigArgs);
 }
 
-export async function createTargetVM(deviceManager: DeviceManager, userConfig: UserConfig): Promise<WARDuinoVM>{
-    const vmConfigArgs = createVMConfigArgs(userConfig);
-    if(userConfig.target === DeploymentMode.DevVM){
-        const maxWaitTime = 3000;
-        const vm = await deviceManager.spawnDevelopmentVM(vmConfigArgs, maxWaitTime);
-        return vm;
+export async function createTargetVM(deviceManager: DeviceManager, program: string, deploymentMode: DeploymentMode, deploy: boolean,  existingToolPort: number | undefined, mcuConfig: UserMCUConnectionConfig | undefined): Promise<WARDuinoVM> 
+{
+    const  vmConfigArgs: VMConfigArgs = {
+        program,
+        disableStrictModuleLoad: true
+    };
+
+    if(deploymentMode === DeploymentMode.DevVM){
+
+        if(deploy){
+            const maxWaitTime = 3000;
+            return await deviceManager.spawnDevelopmentVM(vmConfigArgs, maxWaitTime);
+        } else if(existingToolPort === undefined){
+            throw new Error('existingToolPort is mandatory when connecting to an already deployed DevVM');
+        }
+        else{
+            const da: DeviceConfigArgs = {
+                name: 'la',
+                deploymentMode: DeploymentMode.DevVM
+            };
+            return await deviceManager.connectToExistingDevVM(da, existingToolPort, program, 3000);
+        }
     }
-    else if(userConfig.target === DeploymentMode.MCUVM && userConfig.debuggingMode !== DebuggingMode.outOfThings){
-        const platformConfig = await createPlatformConfig(userConfig, vmConfigArgs);
-        return await deviceManager.spawnHardwareVM(platformConfig);
-    }
-    else {
-        throw new Error(`TODO: Unsupported Deployment mode ${userConfig.target}`);
+    else{
+        if(mcuConfig === undefined){
+            throw new Error('MCU config is mandatory when targetting mcu');
+        }
+
+        vmConfigArgs.serialPort = mcuConfig.serialPort;
+        vmConfigArgs.baudrate = mcuConfig.baudrate;
+        const platformConfig = await createPlatformConfig(mcuConfig, vmConfigArgs);
+        if(deploy){
+            return await deviceManager.spawnHardwareVM(platformConfig);
+        } else{
+            return await deviceManager.connectToExistingMCUVM(platformConfig);
+        }
     }
 }
 
-export async function createDebuggerBackend(devicesManager: DeviceManager, userConfig: UserConfig): Promise<RemoteDebuggerBackend> {
-    const targetVM = await createTargetVM(devicesManager, userConfig);
+
+export async function setupForEdwardDebugging(devicesManager: DeviceManager, config: UserEdwardDebuggingConfig): Promise<RemoteDebuggerBackend>{
+    const targetVM = await createTargetVM(devicesManager, config.programOnTarget, config.target, !!config.deployOnStart, config.toolPortExistingVM, config.mcuConfig);
+    let ooVM: OutOfPlaceVM | undefined;
+    if(config.toolPortExistingVM){
+        ooVM =  await devicesManager.setupAlreadySpawnedVMForOutOfPlaceVM(config.toolPortExistingVM, targetVM, config.serverPortForProxyCall, 10000);
+    }else{
+        ooVM = await devicesManager.spawnOutOfPlaceVM(targetVM, InputMode.RedirectInput);
+    }
+    const dbg = new RemoteDebuggerBackend(ooVM, DebuggingMode.edward);
+    if(!(await ooVM.subscribeOnNewEvent((ev)=>{
+        dbg.onNewEvent(ev);}))) {
+        throw new Error('Could not subscribe to on New IO Event');
+    }
+    return dbg;
+}
+
+
+async function setupForOutOfThingsDebugging(devicesManager:DeviceManager, config: UserOutOfThingsDebuggingConfig): Promise<RemoteDebuggerBackend> {
+    const targetVM = await createTargetVM(devicesManager, config.programOnTarget, config.target, !!config.deployOnStart, config.toolPortExistingVM, config.mcuConfig);
+    const monitor = devicesManager.createOutOfThingsMonitor(targetVM);
+    const dbg = new RemoteDebuggerBackend(targetVM, DebuggingMode.outOfThings, {
+        initialRunningState: RunningState.paused
+    });
+    await dbg.setMonitor(monitor);
+    return dbg;
+}
+
+async function setupForRemoteDebugging(devicesManager:DeviceManager, config: UserRemoteDebuggingConfig): Promise<RemoteDebuggerBackend> {
+    const targetVM = await createTargetVM(devicesManager, config.program, config.target, !!config.deployOnStart, config.toolPortExistingVM, config.mcuConfig);
+    const dbg= new RemoteDebuggerBackend(targetVM, DebuggingMode.remoteDebugging);
+    if(!await targetVM.subscribeOnNewEvent(dbg.onNewEvent.bind(dbg))){
+        throw new Error('Could not subscribe to on New IO Event');
+    }
+    return dbg;
+}
+
+
+
+export async function createDebuggerBackend(devicesManager: DeviceManager, userConfig: UserDeviceConfig): Promise<RemoteDebuggerBackend> {
     if(userConfig.debuggingMode === DebuggingMode.remoteDebugging){
-        const dbg= new RemoteDebuggerBackend(targetVM);
-        if(!await targetVM.subscribeOnNewEvent(dbg.onNewEvent.bind(dbg))){
-            throw new Error('Could not subscribe to on New IO Event');
+        if(userConfig.remoteDebuggingConfig === undefined){
+            throw new Error('remoteDebuggingConfig is missing');
         }
-        return dbg;
+        return await setupForRemoteDebugging(devicesManager, userConfig.remoteDebuggingConfig);
     }
     else if(userConfig.debuggingMode === DebuggingMode.edward){
-        let ooVM: OutOfPlaceVM | undefined;
-        const oopMode: OutOfPlaceMode =  OutOfPlaceMode.RedirectIO;
-        if(userConfig.existingVM !== undefined && userConfig.existingVM && userConfig.toolPortExistingVM !== undefined){
-            ooVM =  await devicesManager.existingVMAsOutOfPlaceVM(userConfig.toolPortExistingVM, targetVM, userConfig.serverPortForProxyCall, 10000);
-        }else{
-            ooVM = await devicesManager.spawnOutOfPlaceVM(targetVM, oopMode);
+        if(userConfig.edwardDebuggingConfig  === undefined){
+            throw new Error('edwardDebuggingConfig is missing');
         }
-        const dbg = new RemoteDebuggerBackend(ooVM);
-        if(!(await ooVM.subscribeOnNewEvent((ev)=>{
-            dbg.onNewEvent(ev);}))) {
-            throw new Error('Could not subscribe to on New IO Event');
-        }
-        return dbg;
+        return await setupForEdwardDebugging(devicesManager, userConfig.edwardDebuggingConfig);
     }
     else if(userConfig.debuggingMode === DebuggingMode.outOfThings){
-
+        if(userConfig.outOfThingsConfig  === undefined){
+            throw new Error('outOfThingsConfig is missing');
+        }
+        return await setupForOutOfThingsDebugging(devicesManager, userConfig.outOfThingsConfig);
     }
     else {
         throw new Error(`unsupported debugging mode ${userConfig.debuggingMode}`);
